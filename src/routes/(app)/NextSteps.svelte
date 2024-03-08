2c5d51ed56c4bd484e2356219f02690eb9145ae7
Timothy Foster <tf.auroratide@gmail.com>
2024-03-07 21:38:29 -0600

    (#6) A social share button, or just generic copy/paste

--- src/routes/(app)/NextSteps.svelte
+++ src/routes/(app)/NextSteps.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import FocusCard from "$lib/FocusCard.svelte"
 	import prompt from "$lib/prompt.svelte"
+	import Share from "./Share.svelte"
 </script>
 
 <FocusCard id="whats-next-section">
@@ -9,24 +10,13 @@
 		<p class="text-center balance more-space-after">Come back tomorrow to see which word won and then invent another new word!</p>
 		{#await prompt.content}
 			<div class="invisible small-stack">
-				{@render share("", "")}
+				<Share word="" definition="" />
 			</div>
 		{:then content}
-			{@render share(prompt.myWord, content.text)}
+			<Share word={prompt.myWord} definition={content.text} />
 		{/await}
 	</div>
 </FocusCard>
-
-{#snippet share(word: string, definition: string)}
-	<div class="small-stack" class:invisible={word == null || word.length === 0}>
-		<p>Share your word on social media:</p>
-		<blockquote>
-			<p>I invented a new word!</p>
-			<p><strong>{word}</strong>: {definition}</p>
-			<p>Invent your own word at fictionary.com.</p>
-		</blockquote>
-	</div>
-{/snippet}
 
 <style>
 	.text-center { text-align: center; }
---
+++ src/routes/(app)/Share.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+	import notifications from "$lib/notifications"
+	import XLogo from "$lib/vendor/XLogo.svelte"
+	import { fly } from "svelte/transition"
+
+	type Props = {
+		word: string
+		definition: string
+	}; const {
+		word,
+		definition,
+	} = $props<Props>()
+
+	const text = $derived(
+		`I invented a new word!\n\n${word}: ${definition}\n\nInvent your own word at fictionary.com.`,
+	)
+
+	let copied = $state(false)
+	let timeout = $state(-1)
+	const copyToClipboard = async () => {
+		window.clearTimeout(timeout)
+		try {
+			await navigator.clipboard?.writeText?.(text)
+			copied = true
+			timeout = window.setTimeout(() => copied = false, 5000)
+		} catch (e) {
+			notifications.error(e as Error)
+		}
+	}
+</script>
+
+<div class="small-stack" class:invisible={word.length === 0}>
+	<p>Share your word on social media:</p>
+	<blockquote>
+		<p>I invented a new word!</p>
+		<p><strong>{word}</strong>: {definition}</p>
+		<p>Invent your own word at fictionary.com.</p>
+	</blockquote>
+	<ul class="row-list">
+		<li>{@render xlink()}</li>
+		<li>{@render copyPaste()}</li>
+	</ul>
+</div>
+
+{#snippet xlink()}
+	<a class="button" href="https://twitter.com/intent/tweet?text={encodeURIComponent(text)}">
+		<XLogo /> Tweet it
+	</a>
+{/snippet}
+
+{#snippet copyPaste()}
+	<button class="position-container" onclick={copyToClipboard}>
+		<span class="define-the-space">Copy/Paste</span>
+		{#if !copied}
+			<span class="transitions" transition:fly={{duration: 120, x: 20 }}>Copy/Paste</span>
+		{:else}
+			<span class="transitions" transition:fly={{duration: 120, x: -20 }}>Copied!</span>
+		{/if}
+	</button>
+{/snippet}
+
+<style>
+	.small-stack {
+		display: flex;
+		flex-direction: column;
+		gap: 0.5em;
+	}
+
+	.invisible { visibility: hidden; }
+
+	.row-list {
+		list-style: none;
+		padding: 0;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+		gap: 1em;
+		flex-wrap: wrap;
+	}
+
+	.position-container { position: relative; }
+	.define-the-space { visibility: hidden; }
+	.transitions {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		position: absolute;
+		inset-block: 0.15em;
+		inset-inline: 0.25em;
+	}
+</style>
