705a83d74b767e6d216c609125154dac4c939e51
Timothy Foster <tf.auroratide@gmail.com>
2024-03-13 18:28:19 -0500

    (#9) Todo legal page

---
+++ src/routes/(app)/legal/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import Stack from "$lib/design-system/Stack.svelte"
+</script>
+
+<svelte:head>
+	<title>Terms of Service</title>
+</svelte:head>
+
+<Stack>
+	<section>
+		<h2>Terms of Service</h2>
+		<p>TODO</p>
+	</section>
+	<section>
+		<h2>Privacy Policy</h2>
+		<p>TODO</p>
+	</section>
+</Stack>
+
+<style>
+	section { inline-size: 100%; }
+
+	p {
+		margin-block-end: 1.5em;
+		line-height: 1.25;
+
+		&:last-child { margin-block-end: 0; }
+	}
+</style>
