66953d4537af9fdf8273aec6268a0be1472329c3
Timothy Foster <tf.auroratide@gmail.com>
2024-03-14 08:25:26 -0500

    (#2) 404 page

---
+++ src/routes/(app)/+error.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { page } from "$app/stores"
+	import FocusCard from "$lib/design-system/FocusCard.svelte"
+	import Stack from "$lib/design-system/Stack.svelte"
+
+	const status = $derived($page.status)
+</script>
+
+<FocusCard error>
+	<Stack>
+		<h2>Apologies!</h2>
+		{#if status === 404}
+			<p>We can't find the page you are looking for.</p>
+		{:else}
+			<p>An error happened. It's our fault, not yours!</p>
+		{/if}
+		<a class="button" href="/">Return Home</a>
+	</Stack>
+</FocusCard>
---
+++ src/routes/(app)/[...path]/+page.ts
@@ -0,0 +1,6 @@
+import { error } from "@sveltejs/kit"
+import type { PageLoad } from "./$types"
+
+export const load: PageLoad = async () => {
+	error(404, "Not found")
+}
