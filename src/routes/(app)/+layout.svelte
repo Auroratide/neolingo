0bc15a1721a3cea3b8a5e31216878926bb5e54d9
Timothy Foster <tf.auroratide@gmail.com>
2024-03-17 19:52:01 -0500

    Disabled for now publicly, so we can set up turnstile possibly?

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -3,33 +3,52 @@
 	import Container from "$lib/design-system/Container.svelte"
 	import Stack from "$lib/design-system/Stack.svelte"
 	import Notifier from "$lib/notifications/Notifier.svelte"
+	import { PUBLIC_ENABLED } from "$env/static/public"
+	import FocusCard from "$lib/design-system/FocusCard.svelte"
 
 	type Props = { children: Snippet }
 	const { children } = $props<Props>()
+
+	const enabled = PUBLIC_ENABLED === "true"
 </script>
 
 <Container>
 	<Stack>
 		<header>
 			<h1>Neolingo</h1>
-			<nav>
-				<ul class="simple-link-list fancy-links min-spacing">
-					<li><a href="/">Invent</a></li>
-					<li><a href="/dictionary">Dictionary</a></li>
-				</ul>
-			</nav>
+			{#if enabled}	
+				<nav>
+					<ul class="simple-link-list fancy-links min-spacing">
+						<li><a href="/">Invent</a></li>
+						<li><a href="/dictionary">Dictionary</a></li>
+					</ul>
+				</nav>
+			{/if}
 		</header>
 		<main>
-			{@render children()}
+			{#if enabled}
+				{@render children()}
+			{:else}
+				<FocusCard>
+					<div class="temp-announcement">
+						<Stack size="1em">
+							<p>Neolingo is under construction!</p>
+							<p>Which means, you can't do much of anything just yet. But <em>eventually</em>, you'll be able to invent some new words!</p>
+						</Stack>
+					</div>
+				</FocusCard>
+			{/if}
 		</main>
 		<footer>
 			<Stack size="0.75em">
-				<ul class="simple-link-list fancy-links max-spacing">
-					<li><a href="/">Invent</a></li>
-					<li><a href="/dictionary">Dictionary</a></li>
-					<li><a href="/accessibility">Accessibility</a></li>
-					<li><a href="/legal">Legal</a></li>
-				</ul>
+				{#if enabled}
+					<ul class="simple-link-list fancy-links max-spacing">
+						<li><a href="/">Invent</a></li>
+						<li><a href="/dictionary">Dictionary</a></li>
+						<li><a href="/accessibility">Accessibility</a></li>
+						<li><a href="/legal">Legal</a></li>
+					</ul>
+				{/if}
 				<p><small>© Auroratide (Timothy Foster), All Rights Reserved.</small></p>
 			</Stack>
 		</footer>
@@ -92,5 +111,12 @@
 		@media (prefers-reduced-motion: reduce) {
 			&::before, &::after { transition: none; }
 		}
+	}
+
+	.temp-announcement {
+		text-align: center;
+		padding-block: 2em;
+		font-size: 1.15em;
+		white-space: balance;
 	}
 </style>
