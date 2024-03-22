fe777cede38ba0e383e222275152a4a289e0ad54
Timothy Foster <tf.auroratide@gmail.com>
2024-03-22 07:26:10 -0500

    (#19) No need to show something broke for non-critical functionality

--- src/routes/Introduction.svelte
+++ src/routes/Introduction.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import WordCard from "$lib/design-system/WordCard.svelte"
 	import WordCardSkeleton from "$lib/design-system/WordCardSkeleton.svelte"
-	import FocusCard from "$lib/design-system/FocusCard.svelte"
 	import * as Api from "$lib/api"
+	import type { OfficialWord } from "$lib/domain"
 
 	const lastWord = Api.getLastWord()
 </script>
@@ -10,18 +10,24 @@
 <section>
 	<p class="text-center large">Each day, <strong>invent a word</strong> for a concept that doesn't have a word yet!</p>
 </section>
-<section class="less-width small-stack">
-	<h2 class="small">Yesterday's word:</h2>
-	{#await lastWord}
-		<WordCardSkeleton />
-	{:then value}
-		<WordCard {value} />
-	{:catch error}
-		<FocusCard error>
-			<p class="text-center block-space">{error.message}</p>
-		</FocusCard>
-	{/await}
-</section>
+{#await lastWord}
+	{@render lastWordSection(undefined)}
+{:then lastWord}
+	{@render lastWordSection(lastWord)}
+{:catch}
+	<!-- nothing -->
+{/await}
+
+{#snippet lastWordSection(word: OfficialWord | undefined)}
+	<section class="less-width small-stack">
+		<h2 class="small">Yesterday's word:</h2>
+		{#if word}
+			<WordCard value={word} />
+		{:else}
+			<WordCardSkeleton />
+		{/if}
+	</section>
+{/snippet}
 
 <style>
 	.less-width { width: min(25em, 95%); }
