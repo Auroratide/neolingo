99014dec8125a477ddd9454715198cf98684f7c9
Timothy Foster <tf.auroratide@gmail.com>
2024-03-28 17:03:35 -0500

    Add horizontal rule and typography to make individual pages more consistent

---
+++ src/lib/design-system/Typography.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import type { Snippet } from "svelte"
+
+	type Props = {
+		children: Snippet,
+	}; const {
+		children,
+	}: Props = $props()
+</script>
+
+<div class="typography">
+	{@render children()}
+</div>
+
+<style>
+	.typography :global(section) {
+		inline-size: 100%;
+	}
+
+	.typography :global(p) {
+		margin-block-end: 1.5em;
+		line-height: 1.25;
+
+		&:last-child { margin-block-end: 0; }
+	}
+
+	.typography :global(h2),
+	.typography :global(h3) {
+		margin-block: 0.25em;
+		color: var(--color-o);
+	}
+</style>
